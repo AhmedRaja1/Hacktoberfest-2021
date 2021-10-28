d61deccaf27075361bcf4d98d4c59a1ec796538b
Abd <abd.arstiae@gmail.com>
2021-10-28 21:21:06 +0500

    Delete JavaScript Library/Discord Music Bot directory

--- JavaScript Library/Discord Music Bot/events/ready.js
+++
@@ -1,5 +0,0 @@
-module.exports = async (client) => {
-    console.log(`Ready on ${client.guilds.cache.size} servers, for a total of ${client.users.cache.size} users`);
-
-    client.user.setActivity(client.config.discord.activity);
-};
